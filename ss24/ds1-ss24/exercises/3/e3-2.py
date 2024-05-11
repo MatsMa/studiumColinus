77775efa4546165fe3550f63d9c2eb4bd99a9f8d
erdfern <rexsomnia@pm.me>
2024-05-11 17:01:54 +0200

    ds1 e3 stuff

--- ss24/ds1-ss24/exercises/3/e3-2.py
+++ ss24/ds1-ss24/exercises/3/e3-2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # a)
-data = np.load("backup_data_2a.npy")
+samples = np.load("backup_data_2a.npy")
 # print(data)
 # print(data.mean())
 
@@ -12,7 +12,7 @@ cov = np.array([[1.0, 0.0], [0.0, 10.0]])
 # cov = np.cov()
 
 # samples = np.random.multivariate_normal(mean, cov, 2000)
-samples = rng.multivariate_normal(mean, cov, 2000, check_valid="raise")
+# samples = rng.multivariate_normal(mean, cov, 2000, check_valid="raise")
 
 # print(samples)
 # print(data == samples)
@@ -31,34 +31,45 @@ r_matrix = get_rotation_matrix(theta)
 # data_rotated = data @ r_matrix
 
 r_matrix = get_rotation_matrix(theta)
-samples_rotated = samples @ r_matrix
+# samples_rotated = samples @ r_matrix
+samples_rotated = np.load("backup_data_2b.npy")
 
 # c)
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
-fig, axes = plt.subplots(1, 2, figsize=(16, 8))
+fig, axes = plt.subplots(1, 2, figsize=(12, 8))
 
-axes[0].annotate(
-    "figure fraction",
-    xy=(0.025, 0.975),
-    xycoords="figure fraction",
-    horizontalalignment="left",
-    verticalalignment="top",
-    fontsize=20,
-)
-axes[0].spines["top"].set_visible(False)
-axes[0].spines["right"].set_visible(False)
-# axes[0].set_xlim(-15, 20)
-axes[0].set_ylim(bottom=0, top=samples.max()) # + 1 to make space for our annotation
+# Plot our data
 axes[0].scatter(samples[:, 0], samples[:, 1], s=8, color="navy")
 axes[0].scatter(
     samples_rotated[:, 0], samples_rotated[:, 1], s=8, color="darkorange"
 )
-mean_axs = axes[0].scatter(0, 0, marker="x", s=50, color="black", label="Mean")
-axes[0].legend(loc="upper left")
+# Insert a marker for mean
+mean_axs = axes[0].scatter(0, 0, marker="x", s=150, color="black", label="Mean")
+
+# Disable top and right plot borders
+axes[0].spines["top"].set_visible(False)
+axes[0].spines["right"].set_visible(False)
+
+# Axis labels
 axes[0].set_xlabel("X")
 axes[0].set_ylabel("Y")
+
+# Ticks in steps of {+-}5
+axes[0].xaxis.set_ticks([tick for tick in axes[0].get_xticks() if tick % 5 == 0])
+axes[0].yaxis.set_ticks([tick for tick in axes[0].get_yticks() if tick % 5 == 0])
+# axes[0].yaxis.set_ticks(np.arange(start, end, 5))
+
+# Show tick values as integers to discard any decimal places
+axes[0].yaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+
+# Titles
+axes[0].set_title("a)", fontsize=16, loc="left")
 axes[0].set_title("Original and rotated point cloud")
+
+# Set legend
+axes[0].legend(loc="upper left", frameon=False)
 
 axes[1].spines["top"].set_visible(False)
 axes[1].spines["right"].set_visible(False)
@@ -77,8 +88,17 @@ rot_data = axes[1].hist(
     label="Rotated data",
 )
 axes[1].set_ylabel("Count")
+
+axes[1].yaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+
+axes[1].xaxis.set_ticks([tick for tick in axes[1].get_xticks() if tick % 5 == 0])
+axes[1].yaxis.set_ticks([tick for tick in axes[1].get_yticks() if tick % 5 == 0])
+
+axes[1].set_title("b)", fontsize=16, loc="left")
 axes[1].set_title("Histogram of the x dimension")
-axes[1].legend(loc="center left")
+
+axes[1].legend(loc="center left", frameon=False)
 
 # plt.tight_layout()
-plt.show()
+# plt.show()
+plt.savefig("icry.jpg")
