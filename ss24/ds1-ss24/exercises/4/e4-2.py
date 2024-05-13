ffff2a43bd6860b1c19670e43769b20c0f95cbfa
erdfern <rexsomnia@pm.me>
2024-05-13 17:06:10 +0200

    e4

--- ss24/ds1-ss24/exercises/4/e4-2.py
+++ ss24/ds1-ss24/exercises/4/e4-2.py
@@ -1,5 +1,22 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("cleaned_titanic.csv", sep=";")
 
 df.info()
+
+print(df["Sex"].unique())
+
+# Further cleaning
+# valid_sex_entries = ["male", 'female', "mal", "femal", "mle"]
+valid_sex_entries = ["male", 'female']
+# Convert all entries to lowercase for comparison
+df['Sex'] = df['Sex'].astype(str).str.lower()
+
+# Replace invalid entries with NaN
+df.loc[~df['Sex'].isin(valid_sex_entries), 'Sex'] = np.nan
+
+# Fill NaN values with 'unknown'
+df['Sex'].fillna('unknown', inplace=True)
+
+print(df)
